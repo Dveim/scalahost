a75dbf8658f1da770aa8073b99f0d00ce9f78bc6
dveim <alexandrolgash@gmail.com>
2015-05-25 21:56:46 +0300

    some research

--- interface/src/main/scala/scala/meta/internal/hosts/scalac/converters/ToMtree.scala
+++ interface/src/main/scala/scala/meta/internal/hosts/scalac/converters/ToMtree.scala
@@ -24,23 +24,89 @@ import scala.meta.dialects.Scala211
 // It is capable of undoing a whole bunch of desugarings performed by scalac,
 // but for some of these desugarings it needs additional tree attachments produced by custom analyzer
 // provided by the scalahost plugin.
-//
-// A lot of heavylifting is performed by the `@converter` macro annotation,
-// which not only simplifies the notation of the converter (by providing convenience .cvt and .cvt_! methods),
-// but also generates a strongly-typed conversion harness, produces boilerplate that simplifies debugging,
-// ensures that all trees are covered by the converter, that there are no conversion ambiguities and much else.
-//
-// Even though this macro annotation does a lot, it's forcing us into
-// a lot of notational weirdness and itself has an implementation whose LOC count
-// surpasses the LOC count of this file. I think that, at a point when we can afford big refactorings,
-// we need to drop the annotation and rewrite everything without its help.
+
 trait ToMtree extends GlobalToolkit with MetaToolkit {
   self: Api =>
 
   def toMtree(gtree: g.Tree): m.Tree = {
-    def correlate(gtree: g.Tree, mtree: m.Tree): m.Tree = (gtree, mtree) match {
-      case _ => ???
+    import g.{Quasiquote => _, _}
+
+    def inside(pat: Any, mtree: m.Tree)(block: => Unit): Unit = {
+      val name = pat.getClass.getSimpleName
+      val hashCode = pat.hashCode()
+
+      println(s"in [$name] [$hashCode]")
+//      println(s"mTree is [$mtree]")
+      block
+      println(s"out [$name] [$hashCode]")
     }
+
+    def correlate(gtree: g.Tree, mtree: m.Tree): Unit /*m.Tree*/ = (gtree, mtree) match {
+      case (pat @ Block(stats: List[Tree], expr: Tree), _mtree) =>
+        inside(pat, _mtree) {
+          stats.foreach(correlate(_, _mtree))
+        }
+
+      case (pat @ ClassDef(mods: Modifiers, name: TypeName, tparams: List[TypeDef], impl: Template), _mtree) =>
+        inside(pat, _mtree) {
+          impl.body.foreach(t => correlate(t.asInstanceOf[g.Tree], _mtree))
+        }
+
+      case (pat @ DefDef(mods: Modifiers, name: TermName, tparams: List[TypeDef], vparams: List[List[ValDef]], tpt: Tree, rhs: Tree), _mtree) =>
+        inside(pat, _mtree) {
+
+        }
+
+      case (pat @ Import(expr, selectors), _mtree) =>
+        inside(pat, _mtree) {
+
+        }
+
+      case (pat @ Literal(value), _mtree) =>
+        inside(pat, _mtree) {
+
+        }
+
+      case (pat @ ModuleDef(mods: Modifiers, name: TermName, impl: Template), _mtree) =>
+        inside(pat, _mtree) {
+
+        }
+
+      case (pat @ PackageDef(pid: RefTree, stats: List[Tree]), _mtree) =>
+        inside(pat, _mtree) {
+          stats.foreach(correlate(_, _mtree))
+        }
+
+      case (pat @ Select(qualifier: Tree, name: Name), _mtree) =>
+        inside(pat, _mtree) {
+
+        }
+
+      case (pat @ ValDef(mods: Modifiers, name: TermName, tpt: Tree, rhs: Tree), _mtree) =>
+        inside(pat, _mtree) {
+
+        }
+
+      case (pat @ TypeTree(), _mtree) =>
+        inside(pat, _mtree) {
+
+        }
+
+      case (EmptyTree, _mtree) =>
+        println("EmptyTree")
+
+//      case _ =>
+//        ???
+    }
+
+    //    val out = correlate(gtree, gtree.pos.source.content.parse[Source].asInstanceOf[m.Tree])
+    //
+    //    println(s"\nout.show[Code] = [${out.show[Code]}]")
+    //    println(s"\nout.show[Raw] = [${out.show[Raw]}]")
+    //    println(s"\nout.show[Semantics] = [${out.show[Semantics]}]")
+    //
+    //    out
     correlate(gtree, gtree.pos.source.content.parse[Source].asInstanceOf[m.Tree])
+    gtree.pos.source.content.parse[Source].asInstanceOf[m.Tree]
   }
 }
